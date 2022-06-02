3b156f33b05494e0f95bcb596c66b635fb99d9df
Matias Sanchez Villar <matuite96@gmail.com>
2022-06-02 14:45:44 -0300

    Dependencias del proyecto

---
+++ back/index.js
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log("http://localhost:" + PORT));
