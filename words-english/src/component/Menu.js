08d6f2e0eba0124bd36e55588abc7a6b3726d45f
Matias Sanchez Villar <matuite96@gmail.com>
2022-07-05 17:43:43 -0300

    Topics

--- words-english/src/component/Menu.js
+++ words-english/src/component/Menu.js
@@ -1,22 +1,10 @@
-import {
-  Toolbar,
-  Typography,
-  AppBar,
-  Button,
-  Box,
-} from "@mui/material";
+import AppBar from '@mui/material/AppBar';
 
-export default function Menu() {
+export default function Menu () {
+
   return (
-    <Box >
-      <AppBar position="sticky">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Words English
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar position="static">
+
+    </AppBar>
   );
-}
+};
