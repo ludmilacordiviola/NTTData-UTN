e9b62ef815cbc13cb6aa3f47ef85d00285fbfb05
Matias Sanchez Villar <matuite96@gmail.com>
2022-07-08 10:14:58 -0300

    useEffect token

--- words-english/src/App.js
+++ words-english/src/App.js
@@ -1,19 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import LoginApp from "./routes/LoginApp";
 import Main from "./routes/Main";
 import Syllabus from "./routes/Syllabus";
 import MenuApp from "./component/MenuApp";
-
+import { useEffect } from "react";
 
 function App() {
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    const token= localStorage.getItem("toke");
+    token ? navigate("/") : navigate("/login") ;
+  }, []); 
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginApp />} />
-        <Route path="/" element={<MenuApp />}>
+        <Route exact path="/" element={<MenuApp />}>
           <Route path="" element={<Main />} />
-          <Route path="Syllabus" element={<Syllabus />} />
+          <Route path="Syllabus" element={<Syllabus />}/>
         </Route>
+        <Route path="/login" element={<LoginApp />} />
       </Routes>
     </BrowserRouter>
   );
