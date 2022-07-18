3074241578e59b40997cb1502eef88de36f58ca8
Matias Sanchez Villar <matuite96@gmail.com>
2022-07-18 15:15:13 -0300

    co

--- words-english/src/utils/userGet.js
+++ words-english/src/utils/userGet.js
@@ -1,10 +1,22 @@
+import axios from "axios";
 
-export default async function getDataUser(token){
-    return await (await fetch(`${process.env.URL}auth/DataUser`, {
-        body: JSON.stringify({token})
-    }))
-    .then(response =>{
-        console.log(response)
+export async function getDataUser(token) {
+  return await 
+    await fetch(`${process.env.URL}auth/DataUser`, {
+      body: JSON.stringify({ token }),
+    })
+    .then((response) => {
+      console.log(response);
     })
     .catch(console.log);
+}
+
+export async function mm(config) {
+  return await axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
